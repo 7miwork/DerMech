d751e097f1e21150107fae5c164f811be447e9e6
7miwork <7miwork@gmail.com>
2026-06-28 17:36:56 +0800

    v1.1.1

---
+++ _seo_helper.py
@@ -0,0 +1,15 @@
+import os
+
+files = [
+    'index.html',
+    'about.html',
+    'services.html',
+    'industries.html',
+    'standards.html',
+    'contact.html',
+]
+base = 'Z:\\Codes\\DerMech'
+marker = '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+ld_marker = '</body>'
+
+tags_map = {}
